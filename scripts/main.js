7c0a48a7b0ba04cb3ea039d4e3b293c5f3dba5a9
Mudwin <nshmilsson@gmail.com>
2025-05-03 23:53:01 +0500

    + new version of API

--- scripts/main.js
+++ scripts/main.js
@@ -4,40 +4,106 @@ import Filters from "./Filters.js";
 new Navigation();
 new Filters();
 
-// Подключение карты
-// --------------------------------------------------------
-ymaps.ready(init);
+initMap();
 
-function init() {
-  const map = new ymaps.Map("map", {
-    center: [56.838281, 60.603365],
-    zoom: 15,
-    controls: [],
+async function initMap() {
+  await ymaps3.ready;
+
+  const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+
+  // Иницилиазируем карту
+  const map = new YMap(document.getElementById("map"), {
+    location: {
+      center: [60.603436, 56.838353],
+      zoom: 15,
+    },
+    zoomRange: { min: 12, max: 20 },
   });
 
-  var myPlacemark = new ymaps.Placemark([56.838281, 60.603365], {
-    balloonContentHeader: "Заголовок",
-    balloonContentBody: "Тело",
-    balloonContentFooter: "Футер",
-    hintContent: "Тест",
-  });
-
-  map.geoObjects.add(myPlacemark);
-
-  setMapLayer(map, "dark");
-}
-
-// Изменение темы карты
-// --------------------------------------------------------
-function setMapLayer(map, map_type) {
-  const MAP = "custom#" + map_type;
-  ymaps.layer.storage.add(MAP, function mapLayer() {
-    return new ymaps.Layer(
-      "https://core-renderer-tiles.maps.yandex.net/tiles?l=map" +
-        (map_type == "dark" ? "&theme=dark" : "") +
-        "&%c&%l"
-    );
-  });
-  ymaps.mapType.storage.add(MAP, new ymaps.MapType(map_type, [MAP]));
-  map.setType(MAP);
+  // Добавляем слой для отображения схематической карты
+  map.addChild(new YMapDefaultSchemeLayer());
+  map.addChild(
+    new YMapDefaultSchemeLayer({
+      theme: "dark",
+      customization: [
+        {
+          tags: {
+            any: ["outdoor", "park", "cemetery", "medical"],
+          },
+          elements: "label",
+          stylers: {
+            visibility: "off",
+          },
+        },
+        {
+          tags: {
+            any: "poi",
+            none: ["outdoor", "park", "cemetery", "medical"],
+          },
+          stylers: {
+            visibility: "off",
+          },
+        },
+        {
+          tags: {
+            any: "road",
+          },
+          types: "point",
+          stylers: {
+            visibility: "off",
+          },
+        },
+        {
+          tags: {
+            any: ["food_and_drink", "shopping", "commercial_services"],
+          },
+          stylers: {
+            visibility: "off",
+          },
+        },
+        {
+          tags: {
+            any: ["traffic_light"],
+          },
+          stylers: {
+            visibility: "off",
+          },
+        },
+        {
+          tags: {
+            any: ["entrance"],
+          },
+          stylers: {
+            visibility: "off",
+          },
+        },
+        {
+          tags: {
+            any: ["road"],
+            none: [
+              "road_1",
+              "road_2",
+              "road_3",
+              "road_4",
+              "road_5",
+              "road_6",
+              "road_7",
+            ],
+          },
+          elements: "label.icon",
+          stylers: {
+            visibility: "off",
+          },
+        },
+        {
+          tags: {
+            any: ["transit"],
+          },
+          stylers: {
+            visibility: "off",
+          },
+        },
+      ],
+    })
+  );
 }
