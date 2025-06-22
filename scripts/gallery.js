ef46735cc39a087d662b92766ed67d83d22d2cfe
Mudwin <nshmilsson@gmail.com>
2025-06-22 12:12:18 +0500

    + gallery's items hover info

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -23,9 +23,21 @@ async function loadGallery() {
       const artImage = `
       <div class="gallery__item">
         <img src="${art.photoURL}" alt="${art.title}" loading="lazy">
+        <div class="gallery__info">
+          <div class="gallery__info-content">
+            <div class="gallery__info-title">
+              ${art.title}
+            </div>
+            <div class="gallery__info-subtitle">${art.district} район</div>
+          </div>
+        </div>
       </div>`;
 
       galleryElement.innerHTML += artImage;
+
+      document.querySelectorAll(".gallery__item").forEach((item) => {
+        item.style.backgroundColor = "#000000";
+      });
     });
   } catch (error) {
     console.error("Ошибка загрузки объектов:", error);
